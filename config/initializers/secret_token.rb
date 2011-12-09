fcb3fc2f96e62e9e00a297eb1e69d79c7531a4f4
Ryan vanniekerk <iamnayr@gmail.com>
2011-12-09 09:22:45 -0800

    Initial rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyWallet::Application.config.secret_token = '6bfeefa3a2ec2b37017e948b1077379f3060ae3a620b91f9ecb3afae3c6dee412793fdd8c386e5695dd5d3adea8f67f56dfca80c18166f5eae94c1803c5b0253'
